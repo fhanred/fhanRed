256532faf30276dd19a0db16cac8de3422718351
daniloherreraf <herreraf.danilo@gmail.com>
2023-10-18 21:42:34 -0500

    Formulario contrato

--- fhanred_fe/src/components/Invoice/Invoice.js
+++ fhanred_fe/src/components/Invoice/Invoice.js
@@ -19,18 +19,23 @@ function Invoice() {
           name: '',
           lastName: '',
           typeId: '' /*tipo documento identificacion*/,
+          typePerson: '',
           email: '',
           Order_date: new Date(),
           birthDate: new Date(),
           CompanyName: '',
-          phone: '',
-          idNum: '' /*plan*/,
+          phone1: '',
+          phone2: '',
+          phone3: '',
+          Product_name: '' /*plan*/,
           n_invoice: 0 /*numero contrato*/,
+          boxNAP: "",
           idStratus: '',
           idZone: '',
           idTypeHouse: '',
           dataMap: new Map(),
           payId: '' /*forma de pago*/,
+          fact_email: '',
           startDate: new Date(),
           cutoffDate: new Date(),
           idLastPayment: '',
@@ -54,6 +59,9 @@ function Invoice() {
           if (values.typeId === 0) {
             errors.typeId = 'Dato Requerido';
           }
+          if (values.typePerson === 0) {
+            errors.typePerson = 'Dato Requerido';
+          }
           if (!values.email) {
             errors.email = 'Por favor ingrese un correo electronico';
           } else if (
@@ -63,6 +71,13 @@ function Invoice() {
           ) {
             errors.email = 'El correo no es valido';
           }
+          if (
+            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+              values.fact_email
+            )
+          ) {
+            errors.fact_email = 'El correo no es valido';
+          }
           if (!values.Order_date) {
             errors.Order_date =
               'Por favor ingrese una fecha formato dd/MM/yyyy';
@@ -71,7 +86,7 @@ function Invoice() {
               values.Order_date
             )
           ) {
-            errors.Order_datee = 'formato de fecha no valido';
+            errors.Order_date = 'formato de fecha no valido';
           }
           if (!values.birthDate) {
             errors.birthDate = 'Por favor ingrese una fecha formato dd/MM/yyyy';
@@ -85,13 +100,23 @@ function Invoice() {
           if (!values.CompanyName) {
             errors.CompanyName = 'Dato Requerido';
           }
-          if (!values.phone) {
-            errors.phone = 'Por favor ingrese un número de celular';
-          } else if (!/^[0-9]{10}$/.test(values.phone)) {
-            errors.phone = 'Por favor ingrese un número valido';
+          if (!values.phone1) {
+            errors.phone1 = 'Por favor ingrese un número de celular';
+          } else if (!/^[0-9]{10}$/.test(values.phone1)) {
+            errors.phone1 = 'Por favor ingrese un número valido';
           }
-          if (values.idNum === 0) {
-            errors.idNum = 'Dato requerido';
+          if (!values.phone2) {
+            errors.phone2 = 'Por favor ingrese un número de celular';
+          } else if (!/^[0-9]{10}$/.test(values.phone2)) {
+            errors.phone2 = 'Por favor ingrese un número valido';
+          }
+          if (!values.phone3) {
+            errors.phone3 = 'Por favor ingrese un número de celular';
+          } else if (!/^[0-9]{10}$/.test(values.phone3)) {
+            errors.phone3 = 'Por favor ingrese un número valido';
+          }
+          if (!values.Product_name) {
+            errors.Product_name = 'Dato requerido';
           }
           if (values.idStratus === 0) {
             errors.idStratus = 'Dato requerido';
@@ -220,6 +245,20 @@ function Invoice() {
                     />
                   </div>
                   <div>
+                    <label htmlFor="typePerson">Tipo de Persona:</label>
+                    <Field id="typePerson" name="typePerson" as="select">
+                      <option value={0}></option>
+                      <option value={1}>Jurídica</option>
+                      <option value={2}>Natural</option>
+                    </Field>
+                    <ErrorMessage
+                      name="typePerson"
+                      component={() => (
+                        <div className={styles.error}>{errors.typePerson}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
                     <label htmlFor="email">Correo Electrónico:</label>
                     <Field
                       type="email"
@@ -235,20 +274,299 @@ function Invoice() {
                     />
                   </div>
                   <div>
-                    <label htmlFor="lastName">Apellidos:</label>
-                    <Field
-                      type="text"
-                      id="lastName"
-                      name="lastName"
-                      placeholder=""
-                    />
+                    <label htmlFor="birthDate">Fecha de Nacimiento:</label>
+                    <Field id="birthDate" name="birthDate">
+                      {({ field, form }) => (
+                        <DatePicker
+                          {...field}
+                          selected={field.value}
+                          onChange={(date) =>
+                            form.setFieldValue(field.name, date)
+                          }
+                          dateFormat="dd/MM/yyyy" // formato de fecha
+                        />
+                      )}
+                    </Field>
                     <ErrorMessage
-                      name="lastName"
+                      name="birthDate"
                       component={() => (
-                        <div className={styles.error}>{errors.lastName}</div>
+                        <div className={styles.error}>{errors.birthDate}</div>
                       )}
                     />
                   </div>
+                  
+                  <div>
+                    <label htmlFor="CompanyName">Razón Social:</label>
+                    <Field
+                      type="text"
+                      id="CompanyName"
+                      name="CompanyName"
+                      placeholder=""
+                    />
+                    <ErrorMessage
+                      name="CompanyName"
+                      component={() => (
+                        <div className={styles.error}>{errors.CompanyName}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone1">Teléfono de Contacto:</label>
+                    <Field
+                      type="tel"
+                      id="phone1"
+                      name="phone1"
+                      placeholder=""
+                    />
+                    <ErrorMessage
+                      name="phone1"
+                      component={() => (
+                        <div className={styles.error}>{errors.phone1}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone2">Teléfono de contacto:</label>
+                    <Field
+                      type="tel"
+                      id="phone2"
+                      name="phone2"
+                      placeholder=""
+                    />
+                    <ErrorMessage
+                      name="phone2"
+                      component={() => (
+                        <div className={styles.error}>{errors.phone2}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone3">Teléfono de contacto:</label>
+                    <Field
+                      type="tel"
+                      id="phone3"
+                      name="phone3"
+                      placeholder=""
+                    />
+                    <ErrorMessage
+                      name="phone3"
+                      component={() => (
+                        <div className={styles.error}>{errors.phone3}</div>
+                      )}
+                    />
+                  </div>
+                </div>
+              )}
+              {step === 2 && (
+                <div>
+                  <h2>Datos Plan </h2>
+                  <div>
+                    <label htmlFor="Product_name">Plan:</label>
+                    <Field
+                      type="text"
+                      id="Product_name"
+                      name="Product_name"
+                      placeholder=""
+                    />
+                    <ErrorMessage
+                      name="Product_name"
+                      component={() => (
+                        <div className={styles.error}>
+                          {errors.Product_name}
+                        </div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="n_invoice">Contrato #:</label>
+                    <Field
+                      type="number"
+                      id="n_invoice"
+                      name="n_invoice"
+                      placeholder=""
+                    />
+                    <ErrorMessage
+                      name="n_invoice"
+                      component={() => (
+                        <div className={styles.error}>{errors.n_invoice}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="idStratus">Estrato:</label>
+                    <Field id="idStratus" name="idStratus" as="select">
+                      <option value={0}></option>
+                      <option value={1}>1</option>
+                      <option value={2}>2</option>
+                      <option value={3}>3</option>
+                      <option value={4}>4</option>
+                      <option value={3}>5</option>
+                      <option value={4}>6</option>
+                    </Field>
+                    <ErrorMessage
+                      name="idStratus"
+                      component={() => (
+                        <div className={styles.error}>{errors.idStratus}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="idZone">Zona:</label>
+                    <Field id="idZone" name="idZone" as="select">
+                      <option value={0}></option>
+                      <option value={1}>Urbana</option>
+                      <option value={2}>Rural</option>
+                    </Field>
+                    <ErrorMessage
+                      name="idZone"
+                      component={() => (
+                        <div className={styles.error}>{errors.idZone}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="idTypeHouse">Tipo de Vivienda:</label>
+                    <Field id="idTypeHouse" name="idTypeHouse" as="select">
+                      <option value={0}></option>
+                      <option value={1}>Alquilada</option>
+                      <option value={2}>Propia</option>
+                      <option value={1}>Familiar</option>
+                    </Field>
+                    <ErrorMessage
+                      name="idTypeHouse"
+                      component={() => (
+                        <div className={styles.error}>{errors.idTypeHouse}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="payId">Forma de Pago:</label>
+                    <Field id="payId" name="payId" as="select">
+                      <option value={0}></option>
+                      <option value={1}>Contado</option>
+                      <option value={2}>Credito</option>
+                    </Field>
+                    <ErrorMessage
+                      name="idTypeHouse"
+                      component={() => (
+                        <div className={styles.error}>{errors.payId}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label>Correo Electrónico envio factura::</label>
+                    <Field
+                      name="toggleField"
+                      render={({ field }) => (
+                        <ToggleButton
+                          value={field.value}
+                          onChange={field.onChange}
+                        />
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">
+                      Correo Electrónico envio factura:
+                    </label>
+                    <Field
+                      type="email"
+                      id="email"
+                      name="email"
+                      placeholder=""
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component={() => (
+                        <div className={styles.error}>{errors.email}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="startDate">Fecha inicio:</label>
+                    <Field id="startDate" name="startDate">
+                      {({ field, form }) => (
+                        <DatePicker
+                          {...field}
+                          selected={field.value}
+                          onChange={(date) =>
+                            form.setFieldValue(field.name, date)
+                          }
+                          dateFormat="dd/MM/yyyy" // formato de fecha
+                        />
+                      )}
+                    </Field>
+                    <ErrorMessage
+                      name="startDate"
+                      component={() => (
+                        <div className={styles.error}>{errors.startDate}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="cutoffDate">Fecha de corte:</label>
+                    <Field id="cutoffDate" name="cutoffDate">
+                      {({ field, form }) => (
+                        <DatePicker
+                          {...field}
+                          selected={field.value}
+                          onChange={(date) =>
+                            form.setFieldValue(field.name, date)
+                          }
+                          dateFormat="dd/MM/yyyy" // formato de fecha
+                        />
+                      )}
+                    </Field>
+                    <ErrorMessage
+                      name="cutoffDate"
+                      component={() => (
+                        <div className={styles.error}>{errors.cutoffDate}</div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="idLastPayment">Fecha de corte:</label>
+                    <Field id="idLastPayment" name="idLastPayment">
+                      {({ field, form }) => (
+                        <DatePicker
+                          {...field}
+                          selected={field.value}
+                          onChange={(date) =>
+                            form.setFieldValue(field.name, date)
+                          }
+                          dateFormat="dd/MM/yyyy" // formato de fecha
+                        />
+                      )}
+                    </Field>
+                    <ErrorMessage
+                      name="idLastPayment"
+                      component={() => (
+                        <div className={styles.error}>
+                          {errors.idLastPayment}
+                        </div>
+                      )}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="salesman">Vendedor:</label>
+                    <Field
+                      type="text"
+                      id="salesman"
+                      name="salesman"
+                      placeholder=""
+                    />
+                    <ErrorMessage
+                      name="salesman"
+                      component={() => (
+                        <div className={styles.error}>{errors.salesman}</div>
+                      )}
+                    />
+                  </div>
+                </div>
+              )}
+              {step === 3 && (
+                <div>
+                  <h2>Equipos</h2>
                 </div>
               )}
             </Form>
