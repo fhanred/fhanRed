08ca474327c180063ddbe7a8dff221feacc6c4fd
daniloherreraf <herreraf.danilo@gmail.com>
2024-01-11 16:58:11 -0500

    push

--- fhanred_Be/src/data/index.js
+++ fhanred_Be/src/data/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = require("../config/envs");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = require("../config/envs");
 //-------------------------------- CONFIGURACION PARA TRABAJAR LOCALMENTE-----------------------------------
 // const sequelize = new Sequelize(
 //   `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
