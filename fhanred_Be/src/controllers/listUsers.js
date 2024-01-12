d70deb60ce41062e14a4ab795ed7a8f1d175d9ac
Guillermo Jofre <jofregf@gmail.com>
2024-01-12 19:14:30 -0300

    feat: creación de la rama, descomentar index para trabajar en local y en listUser agregado de include para ver los Plan y Delivery si crear la ruta

--- fhanred_Be/src/controllers/listUsers.js
+++ fhanred_Be/src/controllers/listUsers.js
@@ -1,8 +1,14 @@
-const { User, Role, Contract } = require("../data");
+const { User, Role, Contract, Plan, Delivery } = require("../data");
 const response = require("../utils/response");
 
 module.exports = async (req, res) => {
   const users = await User.findAll({
+    // include: [
+    //   {
+    //     model: Contract,
+    //     include: [Plan, Delivery], // Incluir la relación con Plan y Delivery en Contract
+    //   },
+    // ],
     include: [{
       model: Contract,
       attributes: ['id_Contract', "estado_contrato", "name_plan", "ultimo_pago"], 
