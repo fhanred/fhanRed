51f1e477ffc8e6f5ed5df802dd25ea0091de316a
mlobeto <mercedeslobeto@gmail.com>
2024-02-03 16:23:16 -0300

    Cambios en getUser

--- fhanred_Be/src/controllers/getUser.js
+++ fhanred_Be/src/controllers/getUser.js
@@ -1,8 +1,19 @@
-const { User} = require("../data");
+const { User, Contract } = require("../data");
 const response = require("../utils/response");
 
 module.exports = async (req, res) => {
-const {n_documento} = req.params;
-  const user = await User.findByPk(n_documento);
-  response(res, 200,  user );
-}; 
+  try {
+    const { n_documento } = req.params;
+    const user = await User.findByPk(n_documento, { include: Contract });
+
+    if (!user) {
+      return response(res, 404, "Usuario no encontrado");
+    }
+
+    return response(res, 200, user);
+  } catch (error) {
+    console.error("Error al obtener usuario por ID:", error);
+    return response(res, 500, "Error interno del servidor");
+  }
+};
+
