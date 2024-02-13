e011d5c999a1bb50ec735f8e9d2f05db15cafa48
Luis Galea <luisgalea31@gmail.com>
2024-02-13 14:01:27 -0400

    fix<fhanred_be\src\app.js>: fix cors

--- fhanred_Be/src/app.js
+++ fhanred_Be/src/app.js
@@ -8,6 +8,7 @@ const { JWT_SECRET_KEY } = require("./config/envs");
 
 
 const app = express();
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
@@ -17,7 +18,6 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.use(cors());
 app.use(morgan("dev"));
 app.use("/", routes);
 app.use((req, res, next) => {
