945dea688e1e2a6c5082872a700337fbcede1c24
Luis Galea <luisgalea31@gmail.com>
2024-02-22 11:46:50 -0400

    feat<fhanred_be\src\data>: change url deploy

--- fhanred_Be/src/data/index.js
+++ fhanred_Be/src/data/index.js
@@ -12,7 +12,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = require("
 //   }
 // );
 // -------------------------------------CONFIGURACION PARA EL DEPLOY---------------------------------------------------------------------
-const sequelize = new Sequelize('postgres://fhanred_user:krA4e8Qh5jcdd1uJRDFa2Zuq3smnrXJH@dpg-cmg645la73kc73f7oka0-a.oregon-postgres.render.com/fhanred', {
+const sequelize = new Sequelize('postgres://fhanred_user:krA4e8Qh5jcdd1uJRDFa2Zuq3smnrXJH@dpg-cmg645la73kc73f7oka0-a/fhanred', {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     }
