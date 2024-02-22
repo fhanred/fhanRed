87afb19b6555a4e951c5f2d8fcb5adc61384e57e
Luis Galea <luisgalea31@gmail.com>
2024-02-22 12:20:53 -0400

    feat<fhanred_be\src\data>: add ssl

--- fhanred_Be/src/data/index.js
+++ fhanred_Be/src/data/index.js
@@ -15,6 +15,12 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = require("
 const sequelize = new Sequelize(DB_DEPLOY, {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      },
     }
 );
 
