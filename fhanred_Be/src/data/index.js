f8d0ba17efecb9040f660eeca0fb5b6d15ef22c8
mlobeto <mercedeslobeto@gmail.com>
2024-02-26 11:36:41 -0300

    Rutas Preparadas para Master

--- fhanred_Be/src/data/index.js
+++ fhanred_Be/src/data/index.js
@@ -4,25 +4,25 @@ const fs = require("fs");
 const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = require("../config/envs");
 //-------------------------------- CONFIGURACION PARA TRABAJAR LOCALMENTE-----------------------------------
- const sequelize = new Sequelize(
-   `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  {
-     logging: false, // set to console.log to see the raw SQL queries
-     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-   }
- );
+//  const sequelize = new Sequelize(
+//    `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+//   {
+//      logging: false, // set to console.log to see the raw SQL queries
+//      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+//    }
+//  );
 // // -------------------------------------CONFIGURACION PARA EL DEPLOY---------------------------------------------------------------------
-//  const sequelize = new Sequelize(DB_DEPLOY, {
-//       logging: false, // set to console.log to see the raw SQL queries
-//       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-//       dialectOptions: {
-//         ssl: {
-//           require: true,
-//           rejectUnauthorized: false,
-//         }
-//       },
-//     }
-// );
+ const sequelize = new Sequelize(DB_DEPLOY, {
+      logging: false, // set to console.log to see the raw SQL queries
+      native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        }
+      },
+    }
+);
  
 const basename = path.basename(__filename);
 
