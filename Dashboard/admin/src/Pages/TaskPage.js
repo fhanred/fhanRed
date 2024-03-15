363963c992e0170a78f3c1368e1392fb4fa6fe77
mlobeto <mercedeslobeto@gmail.com>
2024-03-15 11:17:59 -0300

    Condicional en Task

--- Dashboard/admin/src/Pages/TaskPage.js
+++ Dashboard/admin/src/Pages/TaskPage.js
@@ -1,4 +1,3 @@
-
 import React, { useState, useEffect } from "react";
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -23,13 +22,11 @@ export default function TaskPage() {
   const dispatch = useDispatch();
   const usersData = useSelector((state) => state.usersData);
   const tasks = useSelector((state) => state.tasks);
-  console.log(tasks)
 
   useEffect(() => {
     dispatch(getUsers());
     dispatch(getTasks());
   }, [dispatch]);
-
 
   const handleChangeUser = (event) => {
     setSelectedUser(event.target.value);
@@ -47,34 +44,44 @@ export default function TaskPage() {
   return (
     <Box className="container" sx={{ minWidth: 120 }}>
       <form onSubmit={handleSubmit}>
-      <FormControl fullWidth>
-  <InputLabel id="demo-simple-select-label">Elegir empleado</InputLabel>
-  <Select
-    labelId="demo-simple-select-label"
-    id="demo-simple-select"
-    value={selectedUser}
-    onChange={handleChangeUser}
-  >
-    {usersData
-      .filter(user => user.id_role === 2 || user.id_role === 3) // Filtrar usuarios por roles 2 o 3
-      .map(user => (
-        <MenuItem key={user.n_documento} value={user.n_documento}>{user.name_razonSocial}</MenuItem>
-      ))}
-  </Select>
-</FormControl>
-        <FormControl fullWidth>
-          <InputLabel id="demo-simple-select-label">Elegir tarea</InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            id="demo-simple-select"
-            value={selectedTask}
-            onChange={handleChangeTask}
-          >
-            {tasks.data.tasks.map(task => (
-              <MenuItem key={task.taskId} value={task.taskId}>{task.nameTask}</MenuItem>
-            ))} 
-          </Select>
-        </FormControl>
+        {usersData.length > 0 ? ( // Verificar si hay usuarios disponibles
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Elegir empleado</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={selectedUser}
+              onChange={handleChangeUser}
+            >
+              {usersData
+                .filter(user => user.id_role === 2 || user.id_role === 3)
+                .map(user => (
+                  <MenuItem key={user.n_documento} value={user.n_documento}>{user.name_razonSocial}</MenuItem>
+                ))}
+            </Select>
+          </FormControl>
+        ) : (
+          <p>No hay empleados disponibles</p>
+        )}
+        
+        {tasks.data && tasks.data.tasks.length > 0 ? ( // Verificar si hay tareas disponibles
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Elegir tarea</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={selectedTask}
+              onChange={handleChangeTask}
+            >
+              {tasks.data.tasks.map(task => (
+                <MenuItem key={task.taskId} value={task.taskId}>{task.nameTask}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        ) : (
+          <p>No hay tareas disponibles</p>
+        )}
+        
         <button type="submit">Asignar Tarea</button>
       </form>
     </Box>
