4e8d0184267afcf55dca8b0da4ed4d5030b457fb
mlobeto <mercedeslobeto@gmail.com>
2024-03-26 14:50:25 -0300

    Vuelve a Force

--- fhanred_Be/index.js
+++ fhanred_Be/index.js
@@ -8,7 +8,7 @@ const sendNotifications = require('./src/controllers/sendNotifications.js')
 
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(async () => {
+conn.sync({ force: true }).then(async () => {
   app.listen(PORT, () => {
     console.log(`🚀 listening on port: ${PORT} 🚀`);
   });
@@ -17,7 +17,7 @@ conn.sync({ alter: true }).then(async () => {
   await helpers.chargeVivienda();
   await helpers.chargeUsers();
   await helpers.chargeVivienda();
-  //await helpers.chargeContratos();
+  await helpers.chargeContratos();
   //await helpers.chargeTickets();
 
   // await sendNotifications(); CUIDADO NO DESCOMENTAR HASTA QUE SE ENVIEN LOS DATOS
